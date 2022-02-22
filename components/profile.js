9a55227e2d21067990a10eeb8ad160ba839e0351
joelfrost <joelfrost4@gmail.com>
2022-02-22 14:30:43 +0000

    Added ability to delete posts

--- components/profile.js
+++ components/profile.js
@@ -3,6 +3,7 @@ import { View, ActivityIndicator, ScrollView, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FlatList } from "react-native-gesture-handler";
 import { Text, Card, Input, Button, Avatar } from "react-native-elements";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 class ProfileScreen extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class ProfileScreen extends Component {
       text: "",
       first_name: "",
       last_name: "",
-      email: ""
+      email: "",
     };
   }
 
@@ -49,7 +50,7 @@ class ProfileScreen extends Component {
         this.setState({
           first_name: responseJson.first_name,
           last_name: responseJson.last_name,
-          email: responseJson.email
+          email: responseJson.email,
         });
       });
   };
@@ -98,7 +99,7 @@ class ProfileScreen extends Component {
       .then((response) => {
         console.log(response);
         this.setState({
-          text: ""
+          text: "",
         });
         if (response.status === 201) {
           this.getPosts();
@@ -107,6 +108,28 @@ class ProfileScreen extends Component {
         } else {
           throw "Something went wrong";
         }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  deletePost = async (postID) => {
+    const token = await AsyncStorage.getItem("@session_token");
+    const id = await AsyncStorage.getItem("@id");
+    console.log(
+      "URL: " + `http://localhost:3333/api/1.0.0/user/${id}/${postID}`
+    );
+    return fetch(`http://localhost:3333/api/1.0.0/user/${id}/post/${postID}`, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Authorization": token,
+      },
+    })
+      .then((response) => {
+        this.getPosts();
+        console.log("Post Deleted");
       })
       .catch((e) => {
         console.log(e);
@@ -124,40 +147,50 @@ class ProfileScreen extends Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-        <Avatar
-                size={128}
-                rounded
-                source={'https://randomuser.me/api/portraits/men/36.jpg'}
-              />
-        <Text style={styles.nametext}>{this.state.first_name} {this.state.last_name}</Text>
-        <Input
-          placeholder="Type a post"
-          multiline
-          numberOfLines={5}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-        />
-        <Button title="Post" onPress={this.submitPost} />
+          <Avatar
+            size={128}
+            rounded
+            source={"https://randomuser.me/api/portraits/men/36.jpg"}
+          />
+          <Text style={styles.nametext}>
+            {this.state.first_name} {this.state.last_name}
+          </Text>
+          <Input
+            placeholder="Type a post"
+            multiline
+            numberOfLines={5}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+          />
+          <Button title="Post" onPress={this.submitPost} />
 
-        <FlatList
-          data={this.state.listData}
-          renderItem={({ item }) => (
-            <View>
-              <Card>
-                <Card.Title>
-                  {item.author.first_name} {item.author.last_name}
-                </Card.Title>
-                <Card.Divider />
-                <Text>{item.text}</Text>
-                <Card.Divider style={styles.divider} />
-                <Text>
-                  Likes: {item.numlikes} Time: {item.timestamp}
-                </Text>
-              </Card>
-            </View>
-          )}
-          keyExtractor={(item,index) => index.toString()}
-        />
+          <FlatList
+            data={this.state.listData}
+            renderItem={({ item, index }) => (
+              <View>
+                <Card>
+                  <Ionicons
+                    name={"trash-outline"}
+                    size={16}
+                    onPress={() => {
+                      this.deletePost(item.post_id);
+                    }}
+                  />
+                  <Card.Title>
+                    {item.author.first_name} {item.author.last_name}
+                  </Card.Title>
+
+                  <Card.Divider />
+                  <Text>{item.text}</Text>
+                  <Card.Divider style={styles.divider} />
+                  <Text>
+                    Likes: {item.numlikes} Time: {item.timestamp}
+                  </Text>
+                </Card>
+              </View>
+            )}
+            keyExtractor={(item, index) => item.post_id.toString()}
+          />
         </View>
       </ScrollView>
     );
@@ -167,17 +200,17 @@ class ProfileScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     gap: 10,
-    marginTop: 10
+    marginTop: 10,
   },
   divider: {
     marginTop: 10,
   },
   nametext: {
-    fontWeight: 'bold',
-    fontSize: 25
-  }
+    fontWeight: "bold",
+    fontSize: 25,
+  },
 });
 
 export default ProfileScreen;
