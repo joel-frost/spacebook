f7be85fe3f6d98fda93b4fc839a6b18a4a02f2da
joelfrost <joelfrost4@gmail.com>
2022-02-22 11:52:32 +0000

    Formatted posts in to cards and added posting feature

--- App.js
+++ App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import HomeScreen from './components/home';
-import LoginScreen from './components/login';
-import SignupScreen from './components/signup';
-import LogoutScreen from './components/logout';
+import HomeScreen from "./components/home";
+import LoginScreen from "./components/login";
+import SignupScreen from "./components/signup";
+import LogoutScreen from "./components/logout";
 
 const Stack = createNativeStackNavigator();
 
-class App extends Component{
-    render(){
-        return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Login" component={LoginScreen} />
-                    <Stack.Screen name="Signup" component={SignupScreen} />
-                    <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} /> 
-                    <Stack.Screen name="Logout" component={LogoutScreen} options={{headerShown: false}} />                                          
-                </Stack.Navigator>
-                
-            </NavigationContainer>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Logout"
+              component={LogoutScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 export default App;
--- components/profile.js
+++ components/profile.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { Component } from "react";
+import { View, ActivityIndicator, ScrollView, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FlatList } from "react-native-gesture-handler";
+import { Text, Card, Input, Button, Icon } from "react-native-elements";
 
 class ProfileScreen extends Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class ProfileScreen extends Component {
     this.state = {
       isLoading: true,
       listData: [],
+      text: "",
     };
   }
 
@@ -18,23 +20,24 @@ class ProfileScreen extends Component {
   }
 
   getPosts = async () => {
-    const token = await AsyncStorage.getItem('@session_token');
-    const id = await AsyncStorage.getItem('@id');
+    const token = await AsyncStorage.getItem("@session_token");
+    const id = await AsyncStorage.getItem("@id");
 
     return fetch(`http://localhost:3333/api/1.0.0/user/${id}/post`, {
-      method: 'get',
+      method: "get",
       headers: {
-        'Content-Type': 'application/json',
-        'X-Authorization': token,
+        "Content-Type": "application/json",
+        "X-Authorization": token,
       },
     })
       .then((response) => {
         if (response.status === 200) {
           return response.json();
-        } if (response.status === 400) {
-          throw 'Unable to get posts';
+        }
+        if (response.status === 400) {
+          throw "Unable to get posts";
         } else {
-          throw 'Something went wrong';
+          throw "Something went wrong";
         }
       })
       .then(async (responseJson) => {
@@ -46,28 +49,79 @@ class ProfileScreen extends Component {
       });
   };
 
+  submitPost = async () => {
+    const token = await AsyncStorage.getItem("@session_token");
+    const id = await AsyncStorage.getItem("@id");
+    return fetch(`http://localhost:3333/api/1.0.0/user/${id}/post`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Authorization": token,
+      },
+      body: `{"text": "${this.state.text}"}`,
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 201) {
+          this.getPosts();
+        } else if (response.status === 400) {
+          throw "Unable to submit post";
+        } else {
+          throw "Something went wrong";
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   render() {
     if (this.state.isLoading) {
       return (
         <View>
-          <ActivityIndicator
-            size="large"
-            color="#00ff00"
-          />
+          <ActivityIndicator size="large" color="#00ff00" />
         </View>
       );
     }
     return (
-      <FlatList
-        data={this.state.listData}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.text}</Text>
-          </View>
-        )}
-      />
+      <ScrollView>
+        <Input
+          placeholder="Type a post"
+          multiline
+          numberOfLines={5}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+        <Button title="Post" onPress={this.submitPost} />
+
+        <FlatList
+          data={this.state.listData}
+          renderItem={({ item }) => (
+            <View>
+              <Card>
+                <Card.Title>
+                  {item.author.first_name} {item.author.last_name}
+                </Card.Title>
+                <Card.Divider />
+                <Text>{item.text}</Text>
+                <Card.Divider style={styles.divider} />
+                <Text>
+                  Likes: {item.numlikes} Time: {item.timestamp}
+                </Text>
+              </Card>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  divider: {
+    marginTop: 10,
+  },
+});
 
 export default ProfileScreen;
